1c93e129179d28b542ffdc60b41f7ee41cc4a10d
petros_ <p.kontakis@athlenda.com>
2019-10-13 19:41:27 +0300

    Updates

---
+++ precache-manifest.48b28d9f2b79deea5d5022721be76b42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b00bd0022db9b435b181dec491174ff0",
+    "url": "/marinetraffic/index.html"
+  },
+  {
+    "revision": "758dfc76bb4fb056fd4d",
+    "url": "/marinetraffic/static/css/main.86ef6a74.chunk.css"
+  },
+  {
+    "revision": "c315e0554e1b004fa712",
+    "url": "/marinetraffic/static/js/2.01699c70.chunk.js"
+  },
+  {
+    "revision": "758dfc76bb4fb056fd4d",
+    "url": "/marinetraffic/static/js/main.dd5f5dbd.chunk.js"
+  },
+  {
+    "revision": "571a54513ec542741d89",
+    "url": "/marinetraffic/static/js/runtime-main.bdc213ce.js"
+  }
+]);
